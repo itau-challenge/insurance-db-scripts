bbb7ca5db89740e8652728f9c6f66193b4dc81f5
Heryson Prestes <herysoncp@gmail.com>
2025-01-14 20:35:18 -0300

    feat: database script and model

---
+++ assets/challenge-dml.sql
@@ -0,0 +1,20 @@
+INSERT INTO challenge.status (id, description) VALUES
+    (1, 'Ativo'),
+    (2, 'Inativo'),
+    (3, 'Em revisão'),
+    (4, 'Enviado');
+
+INSERT INTO challenge.insurance_type (id, description) VALUES
+    (1, 'Seguro de vida'),
+    (2, 'Seguro de imóvel'),
+    (3, 'Seguro de automóvel');
+
+INSERT INTO challenge.intention_change_type (id, description) VALUES
+    (1, 'Melhorar cobertura'),
+    (2, 'Redução de custo'),
+    (3, 'Desacordo'),
+    (4, 'Outros');
+
+
+
+
